017b3c548fb9911195cb13b3a25f828b3a87ac59
veso100 <vdavidov@gmail.com>
2013-09-21 17:52:21 +0300

    from/to dvigatel

--- src/tu/sofia/aez/ui/UIDvigatel.java
+++ src/tu/sofia/aez/ui/UIDvigatel.java
@@ -15,6 +15,7 @@ public class UIDvigatel {
 
 	public static final int INPUT_COLUMNS = 7;
 	private final Dvigatel dvigatel;
+
 	private JPanel resultPanel = new JPanel();
 	private JTextField pNTextField = new JTextField(INPUT_COLUMNS);
 	private JTextField U1nTextField = new JTextField(INPUT_COLUMNS);
@@ -29,7 +30,7 @@ public class UIDvigatel {
 	private JTextField WnTextField = new JTextField(INPUT_COLUMNS);
 	private JTextField WoTextField = new JTextField(INPUT_COLUMNS);
 	private JTextField IpnTextField = new JTextField(INPUT_COLUMNS);
-	private JButton editSaveButton=new JButton();
+	private JButton editSaveButton = new JButton();
 	private JLabel pNLabel = new JLabel("Номинална мощност");
 	private JLabel U1nLabel = new JLabel("Номинално напрежение на статора");
 	private JLabel U2nLabel = new JLabel("Номинално напрежение на ротора");
@@ -43,8 +44,8 @@ public class UIDvigatel {
 	private JLabel WnLabel = new JLabel("Номинална ъглова скорост");
 	private JLabel WoLabel = new JLabel("Синхронна ъглова скорост");
 	private JLabel IpnLabel = new JLabel("Ипн");
-	private SaveButtonActionListener saveAction=new SaveButtonActionListener();
-	private EditButtonActionListener editAction=new EditButtonActionListener();
+	private SaveButtonActionListener saveAction = new SaveButtonActionListener();
+	private EditButtonActionListener editAction = new EditButtonActionListener();
 
 	public UIDvigatel(Dvigatel dvg) {
 		this.dvigatel = dvg;
@@ -63,11 +64,11 @@ public class UIDvigatel {
 	}
 
 	public JPanel getEditPanel(boolean editable) {
-		
+
 		resultPanel.setLayout(new MigLayout("center"));
 		JLabel title = new JLabel("Технически унивеситет - София");
 		resultPanel.add(title, "span,align center, wrap");
-
+		fromDvigatel();
 		setEditable(editable);
 		resultPanel.add(pNLabel, "gapy 20");
 		resultPanel.add(pNTextField);
@@ -112,7 +113,7 @@ public class UIDvigatel {
 		return resultPanel;
 	}
 
-	private void setEditable(boolean editable){
+	private void setEditable(boolean editable) {
 		pNTextField.setEditable(editable);
 		U1nTextField.setEditable(editable);
 		U2nTextField.setEditable(editable);
@@ -127,18 +128,51 @@ public class UIDvigatel {
 		WoTextField.setEditable(editable);
 		IpnTextField.setEditable(editable);
 	}
+
+	private void fromDvigatel() {
+		pNTextField.setText(dvigatel.getpN() + "");
+		U1nTextField.setText(dvigatel.getU1n() + "");
+		U2nTextField.setText(dvigatel.getU2n() + "");
+		IoTextField.setText(dvigatel.getIo() + "");
+		ImNTextField.setText(dvigatel.getImN() + "");
+		NnTextField.setText(dvigatel.getNn() + "");
+		NoTextField.setText(dvigatel.getNo() + "");
+		X2TextField.setText(dvigatel.getX2() + "");
+		R1TextField.setText(dvigatel.getR1() + "");
+		R2TextField.setText(dvigatel.getR2() + "");
+		WnTextField.setText(dvigatel.getWn() + "");
+		WoTextField.setText(dvigatel.getWo() + "");
+		IpnTextField.setText(dvigatel.getIpn() + "");
+	}
+
+	private void toDvigatel() {
+		dvigatel.setpN(Double.parseDouble(pNTextField.getText()));
+		dvigatel.setU1n(Double.parseDouble(U1nTextField.getText()));
+		dvigatel.setU2n(Double.parseDouble(U2nTextField.getText()));
+		dvigatel.setIo(Double.parseDouble(IoTextField.getText()));
+		dvigatel.setImN(Double.parseDouble(ImNTextField.getText()));
+		dvigatel.setNn(Double.parseDouble(NnTextField.getText()));
+		dvigatel.setNo(Double.parseDouble(NoTextField.getText()));
+		dvigatel.setX2(Double.parseDouble(X2TextField.getText()));
+		dvigatel.setR1(Double.parseDouble(R1TextField.getText()));
+		dvigatel.setR2(Double.parseDouble(R2TextField.getText()));
+		dvigatel.setWn(Double.parseDouble(WnTextField.getText()));
+		dvigatel.setWo(Double.parseDouble(WoTextField.getText()));
+		dvigatel.setIpn(Double.parseDouble(IpnTextField.getText()));
+	}
+
 	class SaveButtonActionListener implements ActionListener {
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			setEditable(false);
 			editSaveButton.setText("Edit");
+			toDvigatel();
 			editSaveButton.removeActionListener(saveAction);
 			editSaveButton.addActionListener(editAction);
 		}
 	}
 
 	class EditButtonActionListener implements ActionListener {
-
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			setEditable(true);
@@ -146,7 +180,6 @@ public class UIDvigatel {
 			editSaveButton.removeActionListener(editAction);
 			editSaveButton.addActionListener(saveAction);
 		}
-
 	}
 
 }
